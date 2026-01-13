3a0f0e7170eaa6eb3a223ebcb55829e6a4677716
yanxuejun <yanxuejun@gmail.com>
2026-01-13 17:06:15 +0800

    edit page add category link

--- components/CategoryGrid.tsx
+++ components/CategoryGrid.tsx
@@ -42,7 +42,7 @@ export default async function CategoryGrid() {
                 </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {categories.map((cat) => (
+                    {categories.map((cat: any) => (
                         <Link
                             key={cat.id}
                             href={`/trends/${cat.id}`}
